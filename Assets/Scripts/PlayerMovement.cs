637631a29515399ef2a3aa6d000a8f7895f77620
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-09-10 13:51:32 -0300

    Player Rotation
    Player now has the ability to rotate their position to change the direction they are facing.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,24 +10,38 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Object references")]
     [SerializeField] private Rigidbody2D playerRB;
+    [SerializeField] private Camera cam;
 
     private Vector2 movementDir;
+    private Vector2 mousePos;
 
     // Update is called once per frame
     void Update()
     {
-        
+         
     }
 
-    // Update method called at fixed intervals (for physics)
+    // Update method called at fixed intervals (for physics & movement)
     private void FixedUpdate()
     {
+        //moving player
         playerRB.MovePosition(playerRB.position + movementDir * moveSpeed * Time.fixedDeltaTime);
+        
+        //rotating player
+        Vector2 lookDir = mousePos - playerRB.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        playerRB.rotation = angle;
     }
 
     //Method that updates the movement direction of the player
     public void GetMovement(InputAction.CallbackContext input)
     {
         movementDir = input.ReadValue<Vector2>();
+    }
+
+    //Method that gets the direction the player should be facing
+    public void GetDirection(InputAction.CallbackContext input)
+    {
+        mousePos = cam.ScreenToWorldPoint(input.ReadValue<Vector2>());
     }
 }
