90e1329642c901493361b8cad03ffcbd4ff453e5
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-09-11 09:31:17 -0300

    Health Comments
    Added comments to the health class.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -13,12 +13,13 @@ public class Health : MonoBehaviour
     //tracked variables
     private float currentHealth;
 
+    //Method that deals damage to entity
     public bool TakeDamage(float damage)
     {
         //see if evasion happens
         if (!(Random.Range(1f, 100f) <= evasionChance))
         {
-            currentHealth -= damage / damageRes;
+            currentHealth -= damage / damageRes; //applies damage
             return true;
         }
         else
@@ -27,21 +28,25 @@ public class Health : MonoBehaviour
         }
     }
 
+    //method that heals damage on entity
     public void heal(float healthAmount)
     {
         currentHealth += healthAmount;
 
+        //apply boundaries to health
         if (currentHealth > maxHealth)
         {
             currentHealth = maxHealth;
         }
     }
 
+    //method that returns the max health of the entity
     public float GetMaxHealth()
     {
         return maxHealth;
     }
 
+    //method that returns the current health of the entity
     public float GetCurrentHealth()
     {
         return currentHealth; 
