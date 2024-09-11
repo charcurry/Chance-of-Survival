ac85e3ca2714baa1564aedd61e2ab8740660fdc3
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-09-11 15:27:32 -0300

    Update BulletTrail.cs

--- Assets/Scripts/BulletTrail.cs
+++ Assets/Scripts/BulletTrail.cs
@@ -1,13 +1,20 @@
  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class BulletTrail : MonoBehaviour
 {
+    private Vector3 startPos;
+    private Vector3 targetPos;
+    private float progress;
+
+    [SerializeField] private float speed = 40f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position.WithAxis(VectorsExtension.Axis.Z, value: -1);
     }
 
     // Update is called once per frame
